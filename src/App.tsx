1410dc75729d1573b40db7e1febdb5a38efa1cab
Alexander Korobov <dktwho@mail.ru>
2023-07-27 12:15:51 +0300

    add localstorage setItem counter change toString to JSON.stringify

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         setCounter(initCountValue)
     }
     const setLocalStorageHandler = () => {
-        localStorage.setItem('counterValue', counter.toString())
+        localStorage.setItem('counterValue', JSON.stringify(counter))
     }
 
     const getLocalStorageHandler = () => {
